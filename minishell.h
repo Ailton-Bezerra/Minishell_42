176f05cd480a204714aa74cb195e4792518518c8
Caroline Jun <caroline_jun@hotmail.com>
2025-02-03 18:10:45 -0300

    Add files via upload

---
+++ minishell.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cabo-ram <cabo-ram@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 11:51:22 by cabo-ram          #+#    #+#             */
+/*   Updated: 2025/02/03 15:19:54 by cabo-ram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <fcntl.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libraries/libft/libft.h"
+
+# define PROMPT "minishell>$ "
+
+void	minishell_loop(void);
+
+#endif
